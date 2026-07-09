41d67f2309a54f5b9c840476f3afa7bddaedc435
niranjan <nikyniranjan31@gmail.com>
2026-07-09 14:04:00 +0530

    add

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -5,19 +5,19 @@ resource "aws_ssm_parameter" "vpc_id" {
 }
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
-  name  = "/${var.project}/${var.environment}/public"
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
   type  = "StringList"
   value = join(",", module.aws_vpc.public_subnet_ids)
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
-  name  = "/${var.project}/${var.environment}/private"
+  name  = "/${var.project}/${var.environment}/private_subnet_ids"
   type  = "StringList"
   value = join(",", module.aws_vpc.private_subnet_ids)
 }
 
 resource "aws_ssm_parameter" "database_subnet_ids" {
-  name  = "/${var.project}/${var.environment}/database"
+  name  = "/${var.project}/${var.environment}/database_subnet_ids"
   type  = "StringList"
   value = join(",", module.aws_vpc.database_subnet_ids)
 }
