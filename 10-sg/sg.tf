a052d494a0914e07db86ea5820506d1b8e10672c
niky <enugandula.niranjan@gmail.com>
2026-07-10 00:11:27 +0530

    sg

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -47,3 +47,23 @@ module "alb_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+# APP ALB accepting traffic from bastion
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id       = module.bastion_sg.sg_id
+  security_group_id = module.alb_sg.sg_id
+}
+
+# JDOPS-32, Bastion host should be accessed from office n/w
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # it must be our system public ip
+  security_group_id = module.bastion_sg.sg_id
+}
