7a9274cfbb188eac37ce4548945c23b6868b8274
niranjan <nikyniranjan31@gmail.com>
2026-07-20 13:36:20 +0530

    add

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -141,3 +141,23 @@ resource "aws_security_group_rule" "mysql_bastion" {
   source_security_group_id = module.bastion_sg.sg_id
   security_group_id = module.mysql_sg.sg_id
 }
+
+# APP backend accepting traffic from vpn
+resource "aws_security_group_rule" "backend_vpn" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.sg_id
+  security_group_id = module.backend_sg.sg_id
+}
+
+# APP mysql accepting traffic from backend
+resource "aws_security_group_rule" "mysql_backend" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.backend_sg.sg_id
+  security_group_id = module.mysql_sg.sg_id
+}
